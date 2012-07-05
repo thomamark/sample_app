1ac124accb29e55f2a6d390ab456b0243bc88af2
Mark Thomas <mr.thomas@gmail.com>
2012-07-05 16:57:10 -0700

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       #Handle save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -66,6 +66,7 @@ describe "signup" do
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
       
     end
