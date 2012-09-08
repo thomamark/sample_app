fc56aae6417ffb12ce550a9563d3af0cb9c88549
Mark Thomas <mr.thomas@gmail.com>
2012-09-07 19:41:22 -0700

    Adding user microposts

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -29,4 +29,18 @@ require 'spec_helper'
       end
     end
   end
+
+  describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe "as correct user" do
+      before { visit root_path }
+
+      it "should delete a micropost" do
+        expect { click_link "delete" }.should change(Micropost, :count).by(-1)
+      end
+    end
+  end
+  
+
 end
